a5d160d6272cb6aac7709c8f614c29d330c5c044
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2023-01-01 12:08:22 +0700

    Login

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const session = require("express-session");
+const hbs = require("hbs");
+
+const authRouter = require("./components/auth");
 const accountsRouter = require("./components/accounts");
 const productRouter = require("./components/product");
 const categoryRouter = require("./components/category");
@@ -11,6 +15,9 @@ const producerRouter = require("./components/producer");
 const orderRouter = require("./components/order");
 const apiProductRouter = require('./components/product/api');
 
+const passport = require("./components/auth/passport");
+const auth = require("./middlewares/auth");
+const { SESSION_SECRET } = require("./config/index.js");
 
 const app = express();
 
@@ -18,13 +25,27 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.authenticate("session"));
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", accountsRouter);
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next();
+});
+
+app.use("/", authRouter);
 app.use("/accounts", accountsRouter);
 app.use("/product", productRouter);
 app.use("/category", categoryRouter);
@@ -33,8 +54,6 @@ app.use("/order", orderRouter);
 
 // API
 app.use('/api/product', apiProductRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
