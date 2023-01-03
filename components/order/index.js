0eb1b9b346aa014c8758aae35245734796319017
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2023-01-03 20:45:50 +0700

    Merge Orders to dev

--- components/order/index.js
+++ components/order/index.js
@@ -5,4 +5,12 @@ const orderController = require("./orderController");
 
 router.get("/", orderController.list_orders);
 
+router.get("/:id", orderController.detail_order);
+
+router.get("/accept/:id", orderController.accept_order);
+
+router.get("/pending/:id", orderController.pending_order);
+
+router.get("/cancel/:id", orderController.cancel_order);
+
 module.exports = router;
